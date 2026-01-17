e9e4c3fffbd15b006863e7315226f072e6179108
Serge Helly <sergio@localhost-live.home>
2026-01-17 16:14:40 +0100

    main sur poc DnD

--- projects/mosaic-dnd/src/main.tsx
+++ projects/mosaic-dnd/src/main.tsx
@@ -1,44 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { useState } from "react";
-
-import { WorkspaceView } from "./ui/WorkspaceView";
-import { WorkspaceADDebugView } from "./ui/Workspace-AD-DebugView";
-
-import type { Workspace } from "./model/workspace";
-
-const initialWorkspace: Workspace = {
-  containers: {},
-};
-
-function App() {
-  const [workspace, setWorkspace] = useState<Workspace>(initialWorkspace);
-
-  // 🔴 SWITCH TEMPORAIRE ICI
-  const DEBUG_AD = true;
-
-  return DEBUG_AD ? (
-    <WorkspaceADDebugView
-      workspace={workspace}
-      setWorkspace={setWorkspace}
-    />
-  ) : (
-    <WorkspaceView />
-  );
-}
-
-ReactDOM.createRoot(
-  document.getElementById("root")!
-).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-/* 
-// ---------- POC mosaic-dnd ----------
-import React from "react";
-import ReactDOM from "react-dom/client";
 import App from "./App";
 
 ReactDOM.createRoot(
@@ -48,4 +9,3 @@ ReactDOM.createRoot(
     <App />
   </React.StrictMode>
 );
-*/
